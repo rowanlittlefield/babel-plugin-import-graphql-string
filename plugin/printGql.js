f5665d0feb23038d1966d65483f878f1306cf749
Rowan Littlefield <rowanlittlefield@ymail.com>
2020-01-19 08:59:45 -0500

    Refactor printGql

--- plugin/printGql.js
+++ plugin/printGql.js
@@ -1,17 +1,16 @@
 const { print } = require('graphql/language');
 
-const removeChars = str => (
+const removeUnnecessaryChars = str => (
   str.replace(/[\r\n,]/g, '')
     .replace(/\s*([{}:$])\s*/g, (_, capture) => capture)
     .replace(/\s{2,}/g, ' ')
 );
 
-module.exports = (graphqlAST, opts) => {
-  const { trim = false } = opts;
+module.exports = (graphqlAST, { trim = false }) => {
   let graphqlStr = print(graphqlAST);
 
   if (trim) {
-    graphqlStr = removeChars(graphqlStr);
+    graphqlStr = removeUnnecessaryChars(graphqlStr);
   }
 
   return graphqlStr;
