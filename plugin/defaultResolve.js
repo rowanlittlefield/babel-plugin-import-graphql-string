12652d0ec3ee10e84f1360663db7d5f8636771df
Rowan Littlefield <rowanlittlefield@ymail.com>
2020-02-29 18:26:36 -0500

    Refactor defaultResolve.js

--- plugin/defaultResolve.js
+++ plugin/defaultResolve.js
@@ -1,16 +1,15 @@
 const { dirname, parse, resolve } = require('path')
 
 module.exports = (src, file, opts = {}) => {
-  if (opts.aliases) {
+  const { aliases } = opts;
+  
+  if (aliases) {
     const firstDir = parse(src).dir.split('/')[0];
-    const firstMatchingAlias = Object.keys(opts.aliases).find((alias) => {
-      const match = firstDir.match(alias);
-      return match && match[0];
-    });
+    const matchingAlias = Object.keys(aliases).find(alias => alias === firstDir);
     
-    if (firstMatchingAlias) {
-      const aliasPath = opts.aliases[firstMatchingAlias];
-      return resolve(src.replace(firstMatchingAlias, aliasPath));
+    if (matchingAlias) {
+      const aliasPath = aliases[matchingAlias];
+      return resolve(src.replace(matchingAlias, aliasPath));
     }
   }
 
