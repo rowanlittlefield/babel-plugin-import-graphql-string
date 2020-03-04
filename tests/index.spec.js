ac2939142f332f7b41196d634fd98e1e7c2dfbe7
Rowan Littlefield <rowanlittlefield@ymail.com>
2020-03-03 21:00:23 -0500

    Comment in remaining unit tests

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -2,35 +2,35 @@ import pluginTester from 'babel-plugin-tester';
 import path from 'path';
 import plugin from '../plugin/index.js';
 
-// pluginTester({
-//   plugin,
-//   pluginName: 'import-graphql-string',
-//   title: 'default options',
-//   fixtures: path.join(__dirname, 'fixtures/default-options'),
-// });
+pluginTester({
+  plugin,
+  pluginName: 'import-graphql-string',
+  title: 'default options',
+  fixtures: path.join(__dirname, 'fixtures/default-options'),
+});
 
-// pluginTester({
-//   plugin,
-//   pluginName: 'import-graphql-string',
-//   title: 'stripIgnoredCharacters',
-//   fixtures: path.join(__dirname, 'fixtures/strip-ignored-characters'),
-//   pluginOptions: {
-//     stripIgnoredCharacters: true,
-//   },
-// });
+pluginTester({
+  plugin,
+  pluginName: 'import-graphql-string',
+  title: 'stripIgnoredCharacters',
+  fixtures: path.join(__dirname, 'fixtures/strip-ignored-characters'),
+  pluginOptions: {
+    stripIgnoredCharacters: true,
+  },
+});
 
-// pluginTester({
-//   plugin,
-//   pluginName: 'import-graphql-string',
-//   title: 'aliases',
-//   fixtures: path.join(__dirname, 'fixtures/aliases'),
-//   pluginOptions: {
-//     aliases: {
-//       'fixtures': './tests/fixtures',
-//       '@': './tests',
-//     },
-//   },
-// });
+pluginTester({
+  plugin,
+  pluginName: 'import-graphql-string',
+  title: 'aliases',
+  fixtures: path.join(__dirname, 'fixtures/aliases'),
+  pluginOptions: {
+    aliases: {
+      'fixtures': './tests/fixtures',
+      '@': './tests',
+    },
+  },
+});
 
 pluginTester({
   plugin,
